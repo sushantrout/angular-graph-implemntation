e2b4d42524ea43e6ba1bf050fbc3451e3aada46d
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2023-01-23 11:50:19 +0530

    initial commit
    initial commit

---
+++ primeng-chart/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {ChartModule} from 'primeng/chart';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ChartModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ primeng-chart/src/app/serviceses/app-config.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { AppConfig } from '../domain/AppConfig';
+
+@Injectable()
+export class AppConfigService {
+    config: AppConfig = {
+        theme: 'lara-light-blue',
+        dark: false,
+        inputStyle: 'outlined',
+        ripple: true
+    };
+
+    private configUpdate = new Subject<AppConfig>();
+
+    configUpdate$ = this.configUpdate.asObservable();
+
+    updateConfig(config: AppConfig) {
+        this.config = config;
+        this.configUpdate.next(config);
+    }
+
+    getConfig() {
+        return this.config;
+    }
+}
